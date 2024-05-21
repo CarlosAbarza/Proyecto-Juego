aa1a233f3ea6ff829df30575e9eabaa20fbbf034
CarlosAbarza <carlos.abarza.s@mail.pucv.cl>
2024-05-20 23:32:02 -0400

    Intento de subir los archivos

---
+++ GameLluviaMenu/core/src/com/mygdx/game/GameLluviaMenu.java
@@ -0,0 +1,55 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameLluviaMenu extends Game {
+
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private int higherScore;
+    private OrthographicCamera camera;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont(); // use libGDX's default Arial font
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render(); // important!
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public BitmapFont getFont() {
+        return font;
+    }
+
+    public int getHigherScore() {
+        return higherScore;
+    }
+
+    public void setHigherScore(int higherScore) {
+        this.higherScore = higherScore;
+    }
+
+    public OrthographicCamera getCam() {
+        return camera;
+    }
+    
+}
